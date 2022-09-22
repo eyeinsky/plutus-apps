a0f5ea3b714762ee4d41bf44a66d3398f56f1ae7
Markus Läll <markus.lall@iohk.io>
2022-09-22 19:06:47 +0300

    Add ring buffer test

---
+++ marconi/test/Streaming.hs
@@ -0,0 +1,15 @@
+module Streaming where
+
+import Hedgehog (MonadTest, Property, assert, (===))
+import Hedgehog qualified as H
+import Hedgehog.Extras.Stock.IO.Network.Sprocket qualified as IO
+import Hedgehog.Extras.Test qualified as HE
+import Hedgehog.Extras.Test.Base qualified as H
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.Hedgehog (testProperty)
+
+tests :: TestTree
+tests = testGroup "Streaming"
+  [ testProperty "prop_ring_buffer" testRingBuffer ]
+
+testRingBuffer = undefined
