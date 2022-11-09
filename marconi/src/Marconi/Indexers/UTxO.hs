983438a33d36f5d9a9be4fd1bb34dae6235647b4
Markus Läll <markus.lall@iohk.io>
2022-11-09 19:13:47 +0200

    fixup! Add UTxO set indexer

--- marconi/src/Marconi/Indexers/UTxO.hs
+++ marconi/src/Marconi/Indexers/UTxO.hs
@@ -1,12 +1,11 @@
 module Marconi.Indexers.UTxO where
 
 import Data.Function ((&))
-import Data.List (foldl')
 import Data.Set qualified as Set
 
 import Cardano.Api qualified as C
-import Cardano.Streaming
-import Marconi.Streaming.ChainSync
+import Cardano.Streaming (chainSyncEventSource)
+import Marconi.Streaming.ChainSync (rollbackRingBuffer)
 import Streaming.Prelude qualified as S
 
 type UTxO = Set.Set C.TxIn
@@ -22,7 +21,7 @@ toEvent source = S.for source blockToUtxos
   where
     -- Create stream of (used, created) UTxOs by transaction
     -- blockToUtxos :: C.BlockInMode C.CardanoMode -> S.Stream (S.Of (UTxO, UTxO)) IO ()
-    blockToUtxos (C.BlockInMode (C.Block (C.BlockHeader slotNo _ _) txs) _) = let
+    blockToUtxos (C.BlockInMode (C.Block (C.BlockHeader _slotNo _ _) txs) _) = let
       usedCreatedByTx :: [(UTxO, UTxO)]
       usedCreatedByTx = map txToUsedCreated txs
       in S.each usedCreatedByTx
@@ -30,7 +29,7 @@ toEvent source = S.for source blockToUtxos
     -- Extract a set of (used, created) UTxOs from a transaction
     txToUsedCreated :: forall era . C.Tx era -> (UTxO, UTxO)
     txToUsedCreated tx = case tx of
-      C.Tx (C.TxBody (tbc :: C.TxBodyContent C.ViewTx era)) _ -> let
+      C.Tx (txBody@(C.TxBody (tbc :: C.TxBodyContent C.ViewTx era))) _ -> let
 
         -- UTxOs used by the transaction
         usedRegular = Set.fromList $ map fst $ C.txIns tbc :: UTxO
@@ -42,23 +41,29 @@ toEvent source = S.for source blockToUtxos
           _                        -> Set.empty
 
         -- UTxOs created by the transaction
+        txOutToRef' = txOutToRef txBody :: C.TxOut ctx era -> C.TxIn
         createdRegular :: UTxO
-        createdRegular = Set.fromList . map txOutToRef $ C.txOuts tbc
+        createdRegular = txOutsToRefs tx
         createdFromReturnCollateral :: UTxO
         createdFromReturnCollateral = case C.txReturnCollateral tbc of
-          C.TxReturnCollateral _ txOuts -> Set.singleton $ txOutToRef txOuts
+          C.TxReturnCollateral _ txOuts -> Set.singleton $ txOutToRef' txOuts
           _                             -> Set.empty
 
         -- Return a pair of (used, created) UTxOs by the transaction
         in ( usedRegular <> usedFromCollateral <> usedFromReference
            , createdRegular <> createdFromReturnCollateral)
 
-    txOutToRef :: C.TxOut ctx era -> C.TxIn
-    txOutToRef = undefined
+    txOutToRef :: C.TxBody era -> C.TxOut ctx era -> C.TxIn
+    txOutToRef txBody _ = C.TxIn (C.getTxId txBody) undefined
+
+    txOutsToRefs :: C.Tx era -> UTxO
+    txOutsToRefs (C.Tx (txb@(C.TxBody txbc)) _) = Set.fromList $ map (C.TxIn $ C.getTxId txb) $ map fst $ zip [C.TxIx 0 .. ] $ C.txOuts txbc
+
+
 
 indexer
   :: FilePath -> C.NetworkId -> C.ChainPoint
   -> S.Stream (S.Of Event) IO r
 indexer socketPath networkId point = chainSyncEventSource socketPath networkId point
-  & rollbackRingBuffer 2106
+  & rollbackRingBuffer 2160
   & toEvent
