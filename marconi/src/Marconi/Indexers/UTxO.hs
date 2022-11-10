550b72ee58769fef9a5199447c135b10c6c3af4a
Markus Läll <markus.l2ll@gmail.com>
2022-11-10 13:04:29 +0200

    fixup! Add UTxO set indexer

--- marconi/src/Marconi/Indexers/UTxO.hs
+++ marconi/src/Marconi/Indexers/UTxO.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE LambdaCase     #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Marconi.Indexers.UTxO where
 
+import Control.Monad.Trans.Class (lift)
 import Data.Function ((&))
 import Data.Set qualified as Set
 
@@ -12,58 +16,64 @@ type UTxO = Set.Set C.TxIn
 
 type Event = (UTxO, UTxO)
 
--- | Convert a stream of blocks into a stream of (used, created) pairs
--- of UTxOs
+-- | Convert a stream of blocks into a stream of (spent, created) pairs
+-- of UTxO
 toEvent
   :: S.Stream (S.Of (C.BlockInMode C.CardanoMode)) IO r
   -> S.Stream (S.Of Event) IO r
 toEvent source = S.for source blockToUtxos
   where
-    -- Create stream of (used, created) UTxOs by transaction
-    -- blockToUtxos :: C.BlockInMode C.CardanoMode -> S.Stream (S.Of (UTxO, UTxO)) IO ()
+    -- Create stream of (spent, created) UTxO by transaction
+    blockToUtxos :: C.BlockInMode C.CardanoMode -> S.Stream (S.Of (UTxO, UTxO)) IO ()
     blockToUtxos (C.BlockInMode (C.Block (C.BlockHeader _slotNo _ _) txs) _) = let
-      usedCreatedByTx :: [(UTxO, UTxO)]
-      usedCreatedByTx = map txToUsedCreated txs
-      in S.each usedCreatedByTx
+      spentCreatedByTx :: [(UTxO, UTxO)]
+      spentCreatedByTx = map txToSpentCreated txs
+      in S.each spentCreatedByTx
 
-    -- Extract a set of (used, created) UTxOs from a transaction
-    txToUsedCreated :: forall era . C.Tx era -> (UTxO, UTxO)
-    txToUsedCreated tx = case tx of
-      C.Tx (txBody@(C.TxBody (tbc :: C.TxBodyContent C.ViewTx era))) _ -> let
+    -- Extract a set of (spent, created) UTxO from a transaction
+    txToSpentCreated :: forall era . C.Tx era -> (UTxO, UTxO)
+    txToSpentCreated tx = case tx of
+      C.Tx (txb@(C.TxBody (txbc :: C.TxBodyContent C.ViewTx era))) _ -> let
+        txOuts = C.txOuts txbc
+        txOutsLength = toEnum $ length txOuts
+        txId = C.getTxId txb
+        mkTxIn word = C.TxIn txId (C.TxIx word)
 
-        -- UTxOs used by the transaction
-        usedRegular = Set.fromList $ map fst $ C.txIns tbc :: UTxO
-        usedFromCollateral = case C.txInsCollateral tbc of
+        -- UTxO spent by transaction
+        spentRegular = Set.fromList $ map fst $ C.txIns txbc :: UTxO
+        spentCollateral = case C.txInsCollateral txbc of
           C.TxInsCollateral _ txIns -> Set.fromList txIns
           _                         -> Set.empty
-        usedFromReference = case C.txInsReference tbc of
+        spentReference = case C.txInsReference txbc of
           C.TxInsReference _ txIns -> Set.fromList txIns
           _                        -> Set.empty
 
-        -- UTxOs created by the transaction
-        txOutToRef' = txOutToRef txBody :: C.TxOut ctx era -> C.TxIn
+        -- UTxO created by transaction
         createdRegular :: UTxO
-        createdRegular = txOutsToRefs tx
+        createdRegular = Set.fromList $ map mkTxIn [0 .. (txOutsLength - 1)]
         createdFromReturnCollateral :: UTxO
-        createdFromReturnCollateral = case C.txReturnCollateral tbc of
-          C.TxReturnCollateral _ txOuts -> Set.singleton $ txOutToRef' txOuts
-          _                             -> Set.empty
+        createdFromReturnCollateral = case C.txReturnCollateral txbc of
+          C.TxReturnCollateral _ _txOuts -> Set.singleton $ C.TxIn txId (C.TxIx txOutsLength)
+          _                              -> Set.empty
 
-        -- Return a pair of (used, created) UTxOs by the transaction
-        in ( usedRegular <> usedFromCollateral <> usedFromReference
+        -- Return a pair of (spent, created) UTxO by the transaction
+        in ( spentRegular <> spentCollateral <> spentReference
            , createdRegular <> createdFromReturnCollateral)
 
-    txOutToRef :: C.TxBody era -> C.TxOut ctx era -> C.TxIn
-    txOutToRef txBody _ = C.TxIn (C.getTxId txBody) undefined
-
-    txOutsToRefs :: C.Tx era -> UTxO
-    txOutsToRefs (C.Tx (txb@(C.TxBody txbc)) _) = Set.fromList $ map (C.TxIn $ C.getTxId txb) $ map fst $ zip [C.TxIx 0 .. ] $ C.txOuts txbc
-
-
+-- | Fold a stream of (spent, created) UTxO to the set of current UTxO.
+toUtxoAtBlock :: S.Stream (S.Of (UTxO, UTxO)) IO r -> S.Stream (S.Of UTxO) IO r
+toUtxoAtBlock = let
+  loop utxo source = lift (S.next source) >>= \case
+    Left r -> pure r
+    Right ((spent, created), source') -> let
+      utxo' = (utxo `Set.difference` spent) `Set.union` created
+      in S.yield utxo' >> loop utxo' source'
+  in loop Set.empty
 
 indexer
   :: FilePath -> C.NetworkId -> C.ChainPoint
-  -> S.Stream (S.Of Event) IO r
+  -> S.Stream (S.Of UTxO) IO r
 indexer socketPath networkId point = chainSyncEventSource socketPath networkId point
   & rollbackRingBuffer 2160
   & toEvent
+  & toUtxoAtBlock
