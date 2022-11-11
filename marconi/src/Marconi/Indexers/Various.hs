616b7e8ec4b60cc045394a58dad312602d19ff54
Markus Läll <markus.lall@iohk.io>
2022-11-11 19:12:26 +0200

    wip

--- marconi/src/Marconi/Indexers/Various.hs
+++ marconi/src/Marconi/Indexers/Various.hs
@@ -2,8 +2,10 @@
 
 module Marconi.Indexers.Various where
 
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Class (lift)
 import Data.Function ((&))
+import Data.Maybe
 
 import Cardano.Api qualified as C
 import Cardano.Streaming (ChainSyncEvent (RollBackward), chainSyncEventSource)
@@ -12,15 +14,22 @@ import Streaming.Prelude qualified as S
 
 countRollbacks :: FilePath -> C.NetworkId -> C.ChainPoint -> S.Stream (S.Of ()) IO r
 countRollbacks socketPath networkId point = chainSyncEventSource socketPath networkId point
-  & countLoop 0
+  & countLoop 0 Nothing
   where
     countLoop
       :: Integer
+      -> Maybe C.SlotNo
       -> S.Stream (S.Of (ChainSyncEvent (C.BlockInMode C.CardanoMode))) IO r
       -> S.Stream (S.Of ()) IO r
-    countLoop n source = lift (S.next source) >>= \case
+    countLoop n maybeSlotNo source = lift (S.next source) >>= \case
       Left r -> pure r
-      Right (e, source') -> countLoop n' source'
-        where n' = case e of
-                RollBackward{} -> n + 1
-                _              -> n
+      Right (e, source') -> do
+        case e of
+          RollBackward{} -> printSlot maybeSlotNo n >> countLoop (n + 1) undefined source'
+          _              -> countLoop n undefined source'
+
+
+    printSlot maybeSlotNo count = lift $ print (count, maybeSlotNo)
+
+getSlotNo :: C.BlockInMode mode -> C.SlotNo
+getSlotNo (C.BlockInMode (C.Block (C.BlockHeader slotNo' _ _) _) _) = slotNo'
